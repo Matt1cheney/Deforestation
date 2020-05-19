9efd5f3317b4be00ad871e11a3a8c3ecedefddf9
Sam Delay <samuel.delay@gmail.com>
2020-05-18 20:33:58 -0400

    npm audit fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,13 +14,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_DATABASE, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// // Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_DATABASE, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 
 // Start the API server
