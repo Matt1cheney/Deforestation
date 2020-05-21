f4b2583b04297e7c4d69f8e8ac03c398ad7fae0c
Sam Delay <samuel.delay@gmail.com>
2020-05-21 19:44:30 -0400

    dashboard "page" first commit

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import SideMenu from "./SideMenu/SideMenu";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import RegionDisplay from "./Regions/RegionDisplay/RegionDisplay";
+
+
+const Dashboard = () => {
+
+  return (
+    <>
+      <Router>
+        <Row>
+          <Col s={12} md={3}>
+            <SideMenu />
+          </Col>
+          <Col s={12} md={9}>
+            <Switch>
+              <Route exact path="/dashboard/regions">
+                <RegionDisplay />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Router>
+    </>
+  )
+}
+
+export default Dashboard;
