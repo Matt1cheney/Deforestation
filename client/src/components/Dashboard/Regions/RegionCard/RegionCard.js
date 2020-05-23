70daacc87904ed66359ca8707896539bb3cf6218
Sam Delay <samuel.delay@gmail.com>
2020-05-23 11:43:51 -0400

    region components

--- client/src/components/Dashboard/Regions/RegionCard/RegionCard.js
+++ client/src/components/Dashboard/Regions/RegionCard/RegionCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "../style.css";
 
 
 const RegionCard = ( {region} ) => {
@@ -13,6 +15,10 @@ const RegionCard = ( {region} ) => {
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Coordinator: {coordinator}</Card.Subtitle>
         <Card.Text>{description}</Card.Text>
+        <Button className="eventBtn">Events</Button>{" "}
+        <Button className="eventBtn">Sites</Button>{" "}
+        <Button className="eventBtn">Seedlings</Button>{" "}
+        <Button className="eventBtn">Edit Region</Button>
       </Card.Body>
     </Card>
   )
--- client/src/components/Dashboard/Regions/RegionDisplay/RegionDisplay.js
+++ client/src/components/Dashboard/Regions/RegionDisplay/RegionDisplay.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import regions from "../../../../regions.json";
+import regions from "../../../../json data/regions.json";
 import RegionCard from "../RegionCard/RegionCard";
 
 
 function RegionDisplay() {
 
+  const admin = true;
+  const coordinator = false;
+
   console.log(regions)
   return (
+
     <Row>
       {regions.map((region, index) => (
         <Col sm={12} key={index}>
