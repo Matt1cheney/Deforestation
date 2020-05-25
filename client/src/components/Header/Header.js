b9bd2275ad7bdd79c87eac4051508221ffbaeb42
Brian Lipsett <lipsettbrian3@gmail.com>
2020-05-25 16:16:42 -0400

    headerV1

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,34 +8,44 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="header">
-        <Dropdown as={ButtonGroup}>
-          <Button variant="success">Split Button</Button>
+      <>
+        <div className="header">
+          <Dropdown as={ButtonGroup}>
+            <Button variant="success">Get Involved</Button>
 
-          <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+            <Dropdown.Toggle
+              split
+              variant="success"
+              id="dropdown-split-basic"
+            />
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <h1>ReforestNation</h1>
-        <Dropdown as={ButtonGroup}>
-          <Button variant="success">Split Button</Button>
-          <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <h1>ReforestNation</h1>
+          <Dropdown as={ButtonGroup}>
+            <Button variant="success">Get Information</Button>
+            <Dropdown.Toggle
+              split
+              variant="success"
+              id="dropdown-split-basic"
+            />
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
         <div className="right">
           <button className="log">
             <a href="/dashboard"></a>Log In
           </button>
         </div>
-      </div>
+      </>
     );
   }
 }
