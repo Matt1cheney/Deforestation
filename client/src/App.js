f8012e338b3d1e7d1e85764868906a79cb6911d3
Sam Delay <samuel.delay@gmail.com>
2020-05-26 15:18:10 -0400

    moved navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./components/Navbar/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home/Home";
 import Volunteer from './components/GetInvolved/VolunteerForm/volunteer';
@@ -13,7 +12,6 @@ function App() {
     <>
     <Router>
         <>
-          <Navbar />
           <Switch>
           <Route exact path="/" component={ Home } />
             <Route path="/dashboard" component={Dashboard}/>
