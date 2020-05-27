a72cda7d6a61a97e6035a7c732953042a82bcb13
Sam Delay <samuel.delay@gmail.com>
2020-05-27 18:01:56 -0400

    adding api functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,5 +8,25 @@ export default {
 
   getPersons: function() {
     return axios.get("/api/persons")
+  },
+
+  createRegion: function(regionData) {
+    return axios.post("/api/regions", regionData);
+  },
+
+  getRegions: function() {
+    return axios.get("/api/regions")
+  },
+
+  createSite: function(siteData) {
+    return axios.post("/api/sites", siteData)
+  },
+
+  getSites: function() {
+    return axios.get("/api/sites")
+  },
+
+  createEvent: function(eventData) {
+    return axios.post("/api/events", eventData)
   }
 }
