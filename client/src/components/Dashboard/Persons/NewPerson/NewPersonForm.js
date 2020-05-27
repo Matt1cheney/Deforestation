bb1ffafd012b38eb4b680cca7be3859d15057956
Sam Delay <samuel.delay@gmail.com>
2020-05-27 17:58:11 -0400

    update to person form

--- client/src/components/Dashboard/Persons/NewPerson/NewPersonForm.js
+++ client/src/components/Dashboard/Persons/NewPerson/NewPersonForm.js
@@ -6,17 +6,32 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 
-const PersonForm = () => {
+const PersonForm = ({ regions }) => {
+
+  // const availableRegions = regions.filter((data) => {
+  //   return data.coordinator === null;
+  // })
+
   const [formState, setFormState] = useState({
+    region: null,
     name: "",
     email: "",
     phone: "",
-    role: "Volunteer",
+    role: "",
     password: ""
   })
 
   const handleChange = (event) => {
     const { name, value } = event.target
+
+    if( value === "none") {
+      setFormState({
+        ...formState,
+        [name]: null
+      })
+      return
+    }
+
     setFormState({
       ...formState,
       [name]: value
@@ -26,7 +41,7 @@ const PersonForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    if (formState.name === "" || formState.role === "" || formState.email === "") {
+    if (formState.name === "" || formState.email === "") {
       alert("Looks like you forgot one!")
       return
     }
@@ -35,18 +50,21 @@ const PersonForm = () => {
 
     setFormState({
       ...formState,
+      region: null,
       name: "",
       email: "",
       phone: "",
       role: "",
-      confirm: "",
       password: ""
     })
   }
 
+  console.log(formState)
+
 
   return (
     <Form className="formContainer" onSubmit={handleSubmit}>
+      <h1>New Person</h1>
       <Form.Row>
         <Form.Group as={Col} controlId="formName">
           <Form.Label>Full Name</Form.Label>
@@ -77,11 +95,22 @@ const PersonForm = () => {
           <Form.Control name="confirm" value={formState.confirm} onChange={handleChange} />
         </Form.Group>
       </Form.Row>
+      <Form.Group controlId="exampleForm.SelectCustom">
+        <Form.Label>Region</Form.Label>
+        <Form.Control as="select" onChange={handleChange} name="region" custom>
+          <option>none</option>
+          {regions.map((region, index) => (
+            <option key={index} value={region._id}>{region.name}</option>
+          ))}
+        </Form.Control>
+      </Form.Group>
       <Form.Group>
         {["radio"].map((type) => (
-          <div key={`inline-${type}`} className="mb-3" >
+          <div key={`inline-${type}`} className="mb-3" value>
             <Form.Check inline label="Coordinator" type={type} id={`inline-${type}-Coordinator`} name="role" value="Coordinator" onChange={handleChange} />
-            <Form.Check inline label="Admin" type={type} id={`inline-${type}-Admin`} name="role" value="Admin" defaultChecked onChange={handleChange} />
+            <Form.Check inline label="Admin" type={type} id={`inline-${type}-Admin`} name="role" value="Admin" onChange={handleChange} />
+            <Form.Check inline label="Land Owner" type={type} id={`inline-${type}-Admin`} name="role" value="LandOwner" onChange={handleChange} />
+            <Form.Check inline label="Volunteer" type={type} id={`inline-${type}-Admin`} name="role" value="Volunteer" onChange={handleChange} />
           </div>
         ))}
       </Form.Group>
@@ -89,7 +118,7 @@ const PersonForm = () => {
       <Button variant="primary" type="submit">
         Submit
   </Button>
-    </Form>
+    </Form >
   )
 }
 
