d2fc5e0089e99f43a3831d96a6f60f89eacf8108
Brian Lipsett <lipsettbrian3@gmail.com>
2020-05-31 16:34:23 -0400

    test

--- client/src/components/Information/InformationView.js
+++ client/src/components/Information/InformationView.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function InformationView() {
     return (
         <div>
-            <h1>ReforestNation</h1>
+            <h1>ReforestNation.</h1>
         </div>
     )
 }
