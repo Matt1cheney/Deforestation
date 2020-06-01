c95cba7005384b624b8f94e752d8aa823d40f5f4
Brian Lipsett <lipsettbrian3@gmail.com>
2020-05-31 22:00:31 -0400

    old files added some mobile touch ups

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,33 +1,35 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
-import SplitButton from "react-bootstrap/SplitButton";
 import "./Header.css";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
     return (
       <>
         <div className="header">
-          <Dropdown as={ButtonGroup}>
-            <Button variant="success">Get Involved</Button>
-
+          <Dropdown as={ButtonGroup} className="getBtn">
+            <Button href="/getInvolved" variant="success">Get Involved</Button>
             <Dropdown.Toggle
               split
               variant="success"
               id="dropdown-split-basic"
             />
-
             <Dropdown.Menu>
               <Dropdown.Item href="/getInvolved">Volunteer</Dropdown.Item>
               <Dropdown.Item href="#/action-2">Seedlings</Dropdown.Item>
               <Dropdown.Item href="#/action-3">Landowner</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-          <h1>ReforestNation</h1>
-          <Dropdown as={ButtonGroup}>
-            <Button variant="success" href="/information">Get Information</Button>
+          <Link to="/">
+            <h1>ReforestNation</h1>
+          </Link>
+          <Dropdown as={ButtonGroup} className="infoBtn">
+            <Button variant="success" href="/information">
+              Get Information
+            </Button>
             <Dropdown.Toggle
               split
               variant="success"
@@ -41,9 +43,7 @@ export default class Header extends React.Component {
           </Dropdown>
         </div>
         <div className="right">
-          <a href="/dashboard">
-            <button className="log">Log In</button>
-          </a>
+         
         </div>
       </>
     );
