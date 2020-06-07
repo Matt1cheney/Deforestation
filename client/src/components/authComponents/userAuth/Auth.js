e8d73e1edbbd85c98c6b8318acc98595db5d4154
Matthew Cheney <matt1cheney@gmail.com>
2020-06-07 09:44:34 -0400

    deleted unused files, changed auth.js structure

--- client/src/components/authComponents/userAuth/Auth.js
+++ client/src/components/authComponents/userAuth/Auth.js
@@ -31,7 +31,7 @@ export const AuthProvider = ({ children }) => {
 
       if (user === null) { 
         setPending(false);
-        return
+        return history.push("/");
       }
         
         API.getPersonByUid(user.uid)
@@ -54,7 +54,6 @@ export const AuthProvider = ({ children }) => {
   if (pending) {
     return <>Loading...</>;
   }
-// change
   return (
     // provides all children wrapped inside this provider tag the user info.
     <AuthContext.Provider value={currentUser}>
