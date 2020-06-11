82d0c2e8143aead2687caa3dce9dd9d8a9d568fc
Sam Delay <samuel.delay@gmail.com>
2020-06-10 20:54:10 -0400

    fixing img size

--- client/src/components/GetInvolved/Source/Source.js
+++ client/src/components/GetInvolved/Source/Source.js
@@ -26,8 +26,8 @@ const SourceInquire = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <a href="mailto:info@appleseedinitiative.org">Email Us</a>
           </Col>
-          <Col xs={12} md={6} className="resourceImg">
-            <Image src={GreenhouseImg}/>
+          <Col xs={12} md={6}>
+            <Image className="resourceImg" src={GreenhouseImg}/>
           </Col>
         </Row>
         <Row className="resourceRow">
@@ -36,8 +36,8 @@ const SourceInquire = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <a href="mailto:info@appleseedinitiative.org">Email Us</a>
           </Col>
-          <Col xs={12} md={6} className="resourceImg">
-            <Image src={LandImg} className="bottomResourceImg"/>
+          <Col xs={12} md={6} >
+            <Image src={LandImg} className="resourceImg"/>
           </Col>
         </Row>
       </Container>
