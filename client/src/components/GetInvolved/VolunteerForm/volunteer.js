f0c595fe547b05d35c5e675941018b4fdf1a36ac
Sam Delay <samuel.delay@gmail.com>
2020-06-15 12:17:26 -0400

    catch

--- client/src/components/GetInvolved/VolunteerForm/volunteer.js
+++ client/src/components/GetInvolved/VolunteerForm/volunteer.js
@@ -48,7 +48,7 @@ export default class Volunteer extends React.Component {
       .then(res => console.log(res))
       .catch(err => console.log(err))
     })
-
+    .catch(err => console.log(err))
     // TRYING TO REFACTOR NODEMAILER CODE 
 
     // .then((res) => {
