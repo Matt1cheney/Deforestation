d53ee518fa536019d779e02b94a5074a7273def9
Matthew Cheney <matt1cheney@gmail.com>
2020-06-15 13:37:48 -0400

    header fix attempt

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from "react-bootstrap/Nav";
 import "./Header.css";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import  { Link }  from "react-router-dom";
@@ -11,20 +14,29 @@ export default class Header extends React.Component {
       <>
         <div className="header">
           <Dropdown as={ButtonGroup} className="getBtn">
-          <Button variant="success">Get Involved</Button>
+
+          <Button as={Link} to="/getInvolved" variant="success">Get Involved</Button>
+
             <Dropdown.Toggle
               split
               variant="success"
               id="dropdown-split-basic"
             />
             <Dropdown.Menu>
-            <Dropdown.Item href="/getInvolved">Volunteer</Dropdown.Item>
-            <Dropdown.Item href="/resource">Resources</Dropdown.Item>
+
+
+            <NavDropdown.Item as={Link} to="/resource">Resources</NavDropdown.Item>
+
             </Dropdown.Menu>
+
           </Dropdown>
+
           <Link to="/"><h1>ReforestNation</h1></Link>
+
           <Dropdown as={ButtonGroup} className="infoBtn">
-          <Button variant="success"href="/information">Get Information</Button>
+
+          <Button as={Link} variant="success" to="/information">Get Information</Button>
+
             {/* <Dropdown.Toggle
               split
               variant="success"
